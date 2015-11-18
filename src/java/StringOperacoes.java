6510e7ae219541f39dcb3fe495f4400f5dd7c047
LuizFernand0 <luizfernando_santos@hotmail.com>
2015-11-18 00:25:08 -0200

    Update StringOperacoes.java

--- src/java/StringOperacoes.java
+++ src/java/StringOperacoes.java
@@ -1,4 +1,9 @@
 package java;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StringOperacoes {
 
